6766ca266ed95f17b9166f9e587d683786e8b07c
JuliaKolano <juliakolano02@gmail.com>
2025-01-10 13:35:24 +0000

    added user credentials

---
+++ src/main/java/com/example/loginscriptjavafx/UserCredentials.java
@@ -0,0 +1,16 @@
+package com.example.loginscriptjavafx;
+
+import java.util.ArrayList;
+
+public class UserCredentials {
+    // Normally this data would be stored in a database
+    // Stored as an array list for simplicity of this project
+    public static ArrayList<String[]> users = new ArrayList<>();
+
+    // Populate the array list with user credentials
+    public UserCredentials() {
+        users.add(new String[]{"Plutonic", "yKDBBZ4ap"});
+        users.add(new String[]{"Misdrawn", "HJfJTjdl"});
+        users.add(new String[]{"Zoneless", "password"});
+    }
+}

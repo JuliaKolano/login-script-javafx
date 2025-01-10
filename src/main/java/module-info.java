51000281ccc979c848932c7f35c3b09797b7a3c7
JuliaKolano <juliakolano02@gmail.com>
2025-01-10 10:38:45 +0000

    initial JavaFX commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.loginscriptjavafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.loginscriptjavafx to javafx.fxml;
+    exports com.example.loginscriptjavafx;
+}
